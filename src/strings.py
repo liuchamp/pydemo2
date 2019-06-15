b2129d275785adf0429f19b0710f2d95b8da4b71
liuchamp <yexingyun9210@gmail.com>
2019-06-15 12:02:44 +0700

    fix: 🐛 在Python源码文件中，一般需要声明为 utf-8 ，防止文件字符串导致的异常

--- src/strings.py
+++ src/strings.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*
+
 price = 108
-print ("the book's price is %s" % price)
+print("the book's price is %s" % price)
 
 
 user = "Charli"
 age = 8
 # 格式化字符串有两个占位符，第三部分提供2个变量
-print("%s is a %s years old boy" % (user , age))
+print("%s is a %s years old boy" % (user, age))
+
+
+num = -28
+print("num is: %6i" % num)
+print("num is: %6d" % num)
+print("num is: %6o" % num)
+print("num is: %6x" % num)
+print("num is: %6X" % num)
+print("num is: %6s" % num)
